d1d3bab2e7d6527771771b6ad791f27daa7d56ba
Francisco Lopez <franlopbri@gmail.com>
2022-02-27 10:29:24 -0600

    Validation - Path Parameters

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #Python
+from turtle import title
 from typing import Optional
 
 #Pydantic
 from pydantic import BaseModel
 
 #FastAPI
-from fastapi import FastAPI, Query
-from fastapi import Body
+from fastapi import FastAPI, Path, Query, Body
 
 app = FastAPI()
 
@@ -27,10 +27,32 @@ def create_person(person: Person = Body(...)):
     return person
 
 # Validaciones: Query Parameters
-
 @app.get("/person/detail")
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: str = Query(...)
-):
+                    name: Optional[str] = Query(
+                            None, 
+                            min_length=10, 
+                            max_length=50,
+                            title="Person Name",
+                            description="This is the person name, its between 1 and 50 characters"
+                        ),
+                    age: Optional[int] = Query(
+                            None, 
+                            gt=1,
+                            title="Person Age",
+                            description="The is the person age"
+                        )
+                ):
     return {"name": name, "age": age}
+
+# Validaciones: Path Parameters
+@app.get("/person/detail/{person_id}")
+def show_person(
+                    person_id: int = Path(
+                            ..., 
+                            gt=0,
+                            title="Person Id",
+                            description="This is the person Id, its greater than 0"
+                        )
+                ):
+    return {person_id: "It exists!"}
