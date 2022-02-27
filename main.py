3c221e246f1f0362787f40dc34da0a7fa348b6ff
Francisco Lopez <franlopbri@gmail.com>
2022-02-27 10:52:36 -0600

    Validations - Models

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 #Python
-from turtle import title
 from typing import Optional
-from unittest import result
+from enum import Enum
 
 #Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI, Path, Query, Body
 
 app = FastAPI()
+
+#Models
+
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
 
 class Location(BaseModel):
     city: str
@@ -17,11 +26,23 @@ class Location(BaseModel):
     country: str
 
 class Person(BaseModel):
-    first_name: str
-    last_name: str
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+            ...,
+            min_length=1,
+            max_length=50
+        )
+    last_name: str = Field(
+            ...,
+            min_length=1,
+            max_length=50
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=115
+    )
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 @app.get("/")
 def home():
