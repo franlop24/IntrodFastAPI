8c21364889601501b2e4b75ddc6fe6a38107c92f
Francisco Lopez <franlopbri@gmail.com>
2022-02-27 10:04:06 -0600

    Validaciones - Query Parameters

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 #FastAPI
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi import Body
 
 app = FastAPI()
@@ -25,3 +25,12 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+# Validaciones: Query Parameters
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+):
+    return {"name": name, "age": age}
